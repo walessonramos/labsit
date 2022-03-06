0f511b6c2439d1b554926ad445395465aed3d0ea
maps_walesson_ramos <walesson.ramos@maps.com.br>
2022-03-05 22:27:42 -0300

    Novo endpoint dizendo hello

---
+++ src/main/java/io/labsit/bank/api/resource/HelloResource.java
@@ -0,0 +1,15 @@
+package io.labsit.bank.api.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello LabSit!";
+    }
+}
